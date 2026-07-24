6a302b24e8a6971598c8ffa67a0740de6af9b6e8
Th3ThirdMan <davidgkennedymail@gmail.com>
2026-07-24 14:46:54 +0100

    Add nearest bin distance calculation

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
 import requests
+from math import radians, sin, cos, sqrt, atan2
+
+user_lat = 53.3498
+user_lon = -6.2603
+
+def calculate_distance(lat1, lon1, lat2, lon2):
+    earth_radius = 6371000
+    
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+    
+    difference_lat = lat2 - lat1
+    difference_lon = lon2 - lon1
+
+    a = (
+        sin(difference_lat / 2) ** 2
+        + cos(lat1) * cos(lat2) * sin(difference_lon / 2) ** 2
+    )
+    
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    
+    return earth_radius * c
+
+nearest_distance = float("inf")
+nearest_bin = None
 
 url = "https://overpass-api.de/api/interpreter"
 
@@ -41,6 +68,24 @@ for waste_bin in data["elements"]:
     print(f"Street: {waste_bin['tags'].get('object:street', 'Unknown')}")
     print(f"Latitude: {waste_bin['lat']}")
     print(f"Longitude: {waste_bin['lon']}")
+    
+    distance = calculate_distance(
+        user_lat,
+        user_lon,
+        waste_bin["lat"],
+        waste_bin["lon"]
+    )
+    
+    if distance < nearest_distance:
+        nearest_distance = distance
+        nearest_bin = waste_bin
+        
+if nearest_bin:
+    print("\nNearest bin:")
+    print(f"\nStreet: {nearest_bin['tags'].get('object:street', 'Unknown')}")
+    print(f"Latitude: {nearest_bin['lat']}")
+    print(f"Longitude: {nearest_bin['lon']}")
+    print(f"Distance: {nearest_distance:.2f} metres")
     print()
 
 
