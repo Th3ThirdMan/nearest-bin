2da30816a7036c1e93e3930d760e21605f4e2cfd
Th3ThirdMan <davidgkennedymail@gmail.com>
2026-07-26 22:46:50 +0100

    Add browser geolocation support

--- web_app.py
+++ web_app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 from app import (
-    get_bins,
     find_nearest_bin,
-    url,
-    query,
+    get_bins,
     headers,
-    user_lat,
-    user_lon
+    search_radius,
+    url
 )
+
 
 app = Flask(__name__)
 
@@ -26,24 +26,55 @@ def home():
 
 @app.route("/find-bin", methods=["POST"])
 def find_bin():
-    data = get_bins(url, query, headers)
-
     nearest_bin = None
     nearest_distance = None
     error_message = None
     data_source = None
 
+    try:
+        user_lat = float(request.form["latitude"])
+        user_lon = float(request.form["longitude"])
+
+    except (KeyError, TypeError, ValueError):
+        error_message = "Your location could not be read."
+
+        return render_template(
+            "index.html",
+            title="Nearest Bin",
+            nearest_bin=None,
+            nearest_distance=None,
+            error_message=error_message,
+            data_source=None
+        )
+
+    query = f"""
+    [out:json];
+    node["amenity"="waste_basket"]
+    (around:{search_radius},{user_lat},{user_lon});
+    out;
+    """
+
+    data = get_bins(url, query, headers)
+
     if data is not None:
         data_source = data.get("source")
+        bins = data.get("elements", [])
 
-        nearest_bin, nearest_distance = find_nearest_bin(
-            data["elements"],
-            user_lat,
-            user_lon
-        )
+        if bins:
+            nearest_bin, nearest_distance = find_nearest_bin(
+                bins,
+                user_lat,
+                user_lon
+            )
+        else:
+            error_message = (
+                f"No public bins were found within "
+                f"{search_radius} metres."
+            )
     else:
         error_message = (
-            "The bin service is temporarily unavailable. Please try again."
+            "The bin service is temporarily unavailable. "
+            "Please try again."
         )
 
     return render_template(
