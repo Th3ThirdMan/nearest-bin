1fed4a3cdc53abbfed93272705fbb49f41b62079
Th3ThirdMan <davidgkennedymail@gmail.com>
2026-07-30 23:37:21 +0100

    Release FindMyBin as installable PWA

---
+++ static/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "findmybin-v1";
+
+const APP_SHELL = [
+  "/",
+  "/static/css/style.css",
+  "/static/js/app.js",
+  "/static/manifest.json",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(APP_SHELL);
+    }),
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function (cacheName) {
+            return cacheName !== CACHE_NAME;
+          })
+          .map(function (cacheName) {
+            return caches.delete(cacheName);
+          }),
+      );
+    }),
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    }),
+  );
+});
